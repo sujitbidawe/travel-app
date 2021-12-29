6b131e7d9a14f773501b330dc95b74560befa472
sujitbidawe <sujit.bidawe@magicedtech.com>
2021-12-30 01:56:39 +0530

    ADDED: code for handling submission with blank values

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,24 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let city = document.getElementById('name').value
+    // check what text was put into the form fields
+    let city = document.getElementById('name').value;
+    let date = document.getElementById('date').value;
     if (city.length) {
-        // document.getElementById('name').style = "border:2px solid #666666"
-        // document.getElementById('error').innerHTML = "";
+        document.getElementById('name').style = "border:2px solid #666666"
+        document.getElementById('city-error').innerHTML = "";
     } else {
-        // document.getElementById('name').style = "border:2px solid red"
-        // document.getElementById('error').innerHTML = "Please enter a city to proceed further.";
+        document.getElementById('name').style = "border:2px solid red"
+        document.getElementById('city-error').innerHTML = "Please enter a city to proceed further.";
+        return;
+    }
+
+    if (date.length) {
+        document.getElementById('date').style = "border:2px solid #666666"
+        document.getElementById('date-error').innerHTML = "";
+    } else {
+        document.getElementById('date').style = "border:2px solid red"
+        document.getElementById('date-error').innerHTML = "Please enter a date to proceed further.";
         return;
     }
     
